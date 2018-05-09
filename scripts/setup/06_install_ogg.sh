048f5f6b2929fd8257b3a62387435b8744746654
Andrii Karaivanskyi <karayvansky@gmail.com>
2018-05-08 19:51:19 -0700

    Added configuration fixes.

--- scripts/setup/06_install_ogg.sh
+++ scripts/setup/06_install_ogg.sh
@@ -1,2 +1,2 @@
 echo "Installing Oracle Golden Gate..."
-sh ~/fbo_ggs_Linux_x64_shiphome/Disk1/runInstaller -silent -nowait -responseFile `readlink -f ~/gg-install-config.rsp`
+~/fbo_ggs_Linux_x64_shiphome/Disk1/runInstaller -silent -waitForCompletion -responseFile `readlink -f ~/gg-install-config.rsp`
